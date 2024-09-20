b7d3caa3100b67306af4a3c3a6bf85dbd323cfbe
Prachalit Parate <prachalitparate13@gmail.com>
2024-09-20 17:51:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className='flex flex-col h-20 items-center bg-slate-400 '>
+      <p className='text-blue-500 p-5'> Hello </p>
+    </div>
+  )
+}
+
+export default App

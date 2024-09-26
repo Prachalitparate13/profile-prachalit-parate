5e29b0364ffc19987cc2cd9fde942fd26e1ae44e
Prachalit Parate <prachalitparate13@gmail.com>
2024-09-26 20:49:37 +0530

    removed cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import CloudflarePagesFunctions from 'vite-plugin-cloudflare-functions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), CloudflarePagesFunctions()],
+  plugins: [react()],
 })
